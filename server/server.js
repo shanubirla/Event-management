f9cbb503e4af3f5c0d6c1c51dc7dc3e2acf27c67
shanubirla <shanubirlashanubirla@gmail.com>
2026-01-09 16:06:36 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+
 app.use(cors({
   origin: "https://event-management-theta-eight.vercel.app",
   credentials: true,
