fd45f8d558c61c84c3834bab6d931aaba2636d2b
shanubirla <shanubirlashanubirla@gmail.com>
2026-01-09 15:58:27 +0530

    update

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
+
 
 
 axiosInstance.interceptors.request.use((config) => {
--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(cors({
+  origin: "https://event-management-theta-eight.vercel.app",
+  credentials: true,
+}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello World");
